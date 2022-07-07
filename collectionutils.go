b608db24317eefded2b0a9a0c5c1c24f8d3f2404
Jadhav Ganesh <JadhavGanesh2@JohnDeere.com>
2022-07-07 16:05:26 +0530

    initial commit7

--- collectionutils.go
+++ collectionutils.go
@@ -3,3 +3,11 @@ package collectionutils
 func Size(arr []string) int {
 	return len(arr)
 }
+
+func Hello() string {
+	return "Hello, You!"
+  }
+  
+  func Goodbye() string {
+	return "Goodbye for now!"
+  }
